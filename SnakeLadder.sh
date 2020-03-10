56236655d665305daeea06fa44f44eb3399ab4ea
gauravm-bit <gauravwrk01@gmail.com>
2020-03-10 02:46:36 -0700

    ADDED: option for noplay,snake and ladder

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -4,11 +4,27 @@ echo "Welcome to snake ladder problem"
 
 #constants
 PLAYER=1
+NOPLAY=0
+SNAKE=1
+LADDER=2
 
 #variables
 position=0
 
+#dieRoll
 dieRoll=$((RANDOM%6+1))
-
 printf "After rolling a die Player gets value $dieRoll"
 
+#options
+optionCheck=$((RANDOM%3))
+case $optionCheck in
+	$NOPLAY)
+			 NOPLAY=$(($position+$NOPLAY))
+			 ;;
+	$SNAKE)
+			SNAKE=$(($position-$dieRoll))
+			 ;;
+	$LADDER)
+			LADDER=$(($position+$dieRoll))
+			 ;;
+esac
